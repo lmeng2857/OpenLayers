66775fa44bfac1bc730d42a3320df64d1e9c42ef
meng <92019443+lmeng2857@users.noreply.github.com>
2024-04-25 23:15:04 +0100

    add marker on the map

--- src/components/OpenLayers.js
+++ src/components/OpenLayers.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { Map } from "ol";
-import { OSM } from "ol/source";
+import { View, Map, Feature } from "ol";
+import { OSM, Vector as VectorSource } from "ol/source";
 import TileLayer from "ol/layer/Tile";
-import { View } from "ol";
+import VectorLayer from "ol/layer/Vector";
 import { useState, useEffect } from "react";
+import { Point, Circle } from "ol/geom";
+import { fromLonLat } from "ol/proj";
+import Style from "ol/style/Style";
+import Icon from "ol/style/Icon";
+import icon from "../asserts/location-sign-svgrepo-com.svg";
+import star from "../asserts/star.png";
 
 const OpenLayers = () => {
   // mistake 1--------- cannot use useState()
@@ -56,13 +62,59 @@ const OpenLayers = () => {
         new TileLayer({
           source: new OSM(),
         }),
+        // new VectorLayer({
+        //   source: new VectorSource({
+        //     features: [new Feature({ geometry: new Point([-110, 45]) })],
+        //   }),
+        // }),
       ],
       target: "js-map",
       view: new View({
-        center: [0, 0],
-        zoom: 2,
+        center: fromLonLat([-0.118092, 51.509865]),
+        zoom: 10,
       }),
     });
+
+    const marker = new VectorLayer({
+      source: new VectorSource({
+        features: [
+          // new Feature({ geometry: new Point([-430, -220]) }),
+          new Feature({
+            geometry: new Point(
+              fromLonLat([-0.118092, 51.509865], "EPSG:3857")
+            ),
+          }),
+        ],
+      }),
+      style: new Style({
+        image: new Icon({
+          color: "red",
+          src: icon,
+          // width: [512, 512],
+          scale: 0.03,
+        }),
+      }),
+    });
+
+    map.addLayer(marker);
+
+    const marker_2 = new VectorLayer({
+      source: new VectorSource({
+        features: [
+          new Feature({
+            geometry: new Point(fromLonLat([-2.58791, 51.454514], "EPSG:3857")),
+          }),
+        ],
+      }),
+      style: new Style({
+        image: new Icon({
+          color: "red",
+          src: star,
+          scale: 0.03,
+        }),
+      }),
+    });
+    map.addLayer(marker_2);
     // const zoomOut = () => {
     //   const view = map.getView();
     //   const zoom = view.getZoom();
@@ -81,7 +133,7 @@ const OpenLayers = () => {
       <div
         id="js-map"
         className="map"
-        style={{ width: "2000px", height: "2000px" }}
+        style={{ width: "100vw", height: "100vh" }}
       ></div>
     </div>
   );
